c286f7fe0d9d6b26dfb169bb2704a98196fa2b5f
Autumn Blake <125754407+AutumnLeavesFall@users.noreply.github.com>
2026-02-21 16:16:53 -0500

    begin checking users, skip built in system accounts

--- change_users.sh
+++ change_users.sh
@@ -5,3 +5,27 @@ if [ "$(id -u)" -ne 0 ]; then
     echo "users.sh run fail"
     exit 1
 fi
+
+# Allowed user list (borrowed from IRSeC 2025)
+user_list=(
+  "fathertime" "chronos" "aion" "kairos" "merlin" "terminator" "mrpeabody"
+  "jamescole" "docbrown" "professorparadox" "drwho" "martymcFly" "arthurdent"
+  "sambeckett" "loki" "riphunter" "theflash" "tonystark" "drstrange"
+  "bartallen" "whiteteam"
+)
+
+
+while IFS=':' read -r username pass _; do
+    echo "~~~~~"
+    username=$(echo "$username" | tr -d '[:space:]')
+
+    # Skip system accounts (UID < 1000)
+    uid=$(id -u "$username" 2>/dev/null)
+    if [ -z "$uid" ] || [ "$uid" -lt 1000 ]; then
+        continue
+    fi
+
+
+
+# Run using the /etc/passwd file
+done < /etc/passwd
