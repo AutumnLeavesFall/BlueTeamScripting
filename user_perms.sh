0e4e95f8592a28ab4a049de68c3e626fa8fcc3c3
Autumn Blake <125754407+AutumnLeavesFall@users.noreply.github.com>
2026-04-01 18:40:33 -0400

    fill out user_perms following plan

--- user_perms.sh
+++ user_perms.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+### NEEDS TESTING ###
 
 # Check if running as root
 if [ "$(id -u)" -ne 0 ]; then
@@ -8,9 +9,17 @@ fi
 
 ## PLAN
 # run user_group.sh
+./user_group.sh
 # ask for directory path
+echo "Please enter a directory to change perms within: "
+read dirpath
 # run recursive (-R) chown to make preferred user group the owner of all files in directory
+chown -R :preferreduser dirpath
 # run recursive chmod so only preferred user has r/w/e perms for all files in directory
+chmod -R 070 dirpath #need to confirm this actually allows preferred user to access and run files but no one else
+
+# need to figure out if I want more here
+
 
 
 ## GOAL
