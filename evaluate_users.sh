219114fabf331cd807c5c113108a488f5c52ebe4
AutumnLeavesFall <125754407+AutumnLeavesFall@users.noreply.github.com>
2026-04-08 14:23:41 +0000

    clean up code

--- evaluate_users.sh
+++ evaluate_users.sh
@@ -20,18 +20,8 @@ while IFS=':' read -r username pass _; do
     uid=$(id -u "$username" 2>/dev/null)
     if [ -z "$uid" ] || [ "$uid" -lt 1000 ]; then
         echo "UID:$uid indicates '$username' is a system account."
-        # read -p "Remove this account? y/n" sysaccrem ## why aren't you working????? this is the recommended way to read user input, tf?????
-        # echo "Remove this account? y/n"
         read -p "Remove this account? y/n: " sysaccrem </dev/tty
         echo "input: $sysaccrem"
-
-        # ~~~~~
-        # UID:117 indicates 'rtkit' is a system account.
-        # Remove this account? y/n
-        # input: colord:x:118:120:colord colour management daemon:/var/lib/colord:/usr/sbin/nologin
-        # ~~~~~
-        # aha! okay so the default input for the entire script is the etc/passwd file, therefore it's reading from that anytime i call read on user input
-
 
         # if [[sysaccrem == "y"]]; then
         #     echo "Removing $username..."
