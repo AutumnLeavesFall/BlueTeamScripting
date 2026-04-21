a6dcc83e396937e8b33371a66e8eda4886a5ad89
Autumn Blake <125754407+AutumnLeavesFall@users.noreply.github.com>
2026-04-21 18:29:13 -0400

    need to test

--- untested/ssh_keys.sh
+++ untested/ssh_keys.sh
@@ -5,15 +5,20 @@ if [ "$(id -u)" -ne 0 ]; then
     echo "ssh_keys.sh run fail"
     exit 1
 fi
-
-read -r key <~/.ssh/authorized_keys
-echo "Key: $key"
-read -p "Remove key? y/n " removekey </dev/tty
-case $removekey in 
-    [Yy] )
-        #
-        ;;
-    [Nn] )
-        echo "Key approved."
-        ;;
+keylist=""
+while IFS= read -r key; do
+    echo "Key: $key"
+    read -p "Remove key? y/n " removekey </dev/tty
+    case $removekey in 
+        [Yy] )
+            echo "Removing key..."
+            ;;
+        [Nn] )
+            echo "Key approved."
+            keylist += $key + "\n"
+            ;;
+        * )
+            echo "Invalid response. Please try again"
+            ;;
     esac
+done < ~/.ssh/authorized_keys
