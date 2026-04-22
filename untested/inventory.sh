489daf77fd62ba31497badad448ef4c25f370895
AutumnLeavesFall <125754407+AutumnLeavesFall@users.noreply.github.com>
2026-04-22 01:49:01 +0000

    inventory in progress: os, version, ip, and mac

--- untested/inventory.sh
+++ untested/inventory.sh
@@ -6,3 +6,44 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
+echo "Gathering Inventory..."
+echo -e "\n~~~~~\n"
+
+
+
+
+# List OS and Version
+echo -n "Operating System: "
+cat /etc/os-release | grep "NAME" | grep -o "\"[a-zA-Z]*\"" | grep -o "[a-zA-Z]*"
+echo -n "Version: "
+cat /etc/os-release | grep "VERSION=" | grep -o "\".*\"" | grep -o "[^\"]*"
+echo -n "Hostname: "
+hostname
+
+echo -e "\n~~~~~\n"
+
+
+# IP and MAC address
+echo -n "IP Address: "
+ip addr | grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | grep -v "127.0.0.1"
+
+echo -n "MAC Address: "
+ip -brief link | grep -o "[0-9a-fA-F]*:[0-9a-fA-F]*:[0-9a-fA-F]*:[0-9a-fA-F]*:[0-9a-fA-F]*:[0-9a-fA-F]*" | grep -v "00:00:00:00:00:00"
+
+echo -e "\n~~~~~\n"
+
+
+# Admin Users
+echo "Admin Users"
+
+echo -e "\n~~~~~\n"
+
+
+# Users
+echo "All Users"
+
+echo -e "\n~~~~~\n"
+
+
+# Open Ports
+echo "Open Ports"
